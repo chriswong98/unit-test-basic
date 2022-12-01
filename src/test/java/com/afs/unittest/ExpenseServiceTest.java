21c3129eabd82e8af623a429986125c65b8c03f2
WONGCH9 <wongch9@oocl.com>
2022-12-01 11:38:03 +0800

    test: added should_return_other_expense_type_if_project_is_external_and_has_other_name

--- src/test/java/com/afs/unittest/ExpenseServiceTest.java
+++ src/test/java/com/afs/unittest/ExpenseServiceTest.java
@@ -53,10 +53,15 @@ class ExpenseServiceTest {
     @Test
     void should_return_other_expense_type_if_project_is_external_and_has_other_name() {
         // given
+        Project project = new Project(ProjectType.EXTERNAL, "Other name");
+        ExpenseService expenseService = new ExpenseService();
 
         // when
+        ExpenseType expenseType = expenseService.getExpenseCodeByProjectTypeAndName(project);
+
 
         // then
+        assertEquals(ExpenseType.OTHER_EXPENSE, expenseType);
 
     }
 
